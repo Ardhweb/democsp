c131d1c36de2f0337ae715d9c3aa486c2e6b87b0
Rahul Mahawar <sun.projectsdev@gmail.com>
2026-05-26 13:30:31 +0530

    last work still bug

---
+++ scribework/data_pipeline.py
@@ -0,0 +1,11 @@
+#ETL - process to load  upload file data into  respective tables into db with clean.
+
+
+def extract_data_process():
+	pass
+
+def clean_data_process():
+	pass
+
+def load_to_db():
+	pass
