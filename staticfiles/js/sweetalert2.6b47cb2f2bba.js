6aeebaf609051c3c2dc827b48c5c280e218032b7
Rahul Mahawar <sun.projectsdev@gmail.com>
2026-05-26 12:32:20 +0530

    sttaic files and alert added

---
+++ staticfiles/js/sweetalert2.6b47cb2f2bba.js
@@ -0,0 +1,22 @@
+import Swal from 'sweetalert2'
+
+// or via CommonJS
+const Swal = require('sweetalert2')
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+import 'sweetalert2/src/sweetalert2.scss'
+import 'sweetalert2/themes/bootstrap-5.css'
+
+Swal.fire({
+  title: 'Bootstrap 5 theme',
+  theme: 'bootstrap-5'
+  // theme: 'bootstrap-5-light' // light theme only
+  // theme: 'bootstrap-5-dark' // dark theme only
+})
+function testAlert() {
+    Swal.fire({
+        title: 'Bootstrap 5 theme',
+        text: 'SweetAlert2 is working!',
+        icon: 'success',
+        confirmButtonText: 'OK'
+    })
+}
